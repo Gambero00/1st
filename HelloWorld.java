a4e04a811ff354477c528adcc5a1a10242d9096a
Gambero00 <131058937+Gambero00@users.noreply.github.com>
2024-03-05 10:40:36 +0100

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+
+public class HelloWorld{
+
+    public static void main(String args[]){
+
+        System.out.println("hello niggera");
+		int c=0;
+		for(int i=1; i<10; i++) {
+			
+				System.out.println("sex");
+				}
+    }
+
+
